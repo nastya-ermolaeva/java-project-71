6ab41eb38302731cc493517a812c91bcf7b23e5e
nastya-ermolaeva <nastyaermolaeva31@mail.ru>
2025-06-04 22:23:45 +0300

    modify build.gradle.kts and App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+    name = "gendiff",
+    mixinStandardHelpOptions = true,
+    version = "1.0",
+    description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable {
+
+    @Override
+    public void run() {
         System.out.printf("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
